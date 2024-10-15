94c5b259235befa7671e3a57c39eb3263efe62bc
Anderson Carvalho <andersoninnocencio17@gmail.com>
2024-10-15 11:47:05 +0100

    feat: configure Game World

--- GAME/src/gameManager/ControlStatus.java
+++ GAME/src/gameManager/ControlStatus.java
@@ -1,8 +1,10 @@
 package gameManager;
 
+import java.awt.*;
+
 //10. Classe HUD
 // Gerencia a interface de usuário (vida, pontuação).
-public class HUD {
+public class ControlStatus {
     public void render(Graphics g, Player player) {
         // Desenhar barra de vida
         g.setColor(Color.GREEN);
---
+++ GAME/src/gameManager/InputHandler.java
@@ -0,0 +1,25 @@
+package gameManager;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
+
+
+import java.awt.event.KeyEvent;
+import java.util.HashSet;
+import java.util.Set;
+
+
+// 8. Classe InputHandler
+// Gerencia as entradas do teclado.
+public class InputHandler implements KeyboardHandler {
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
+
+    }
+}
+
